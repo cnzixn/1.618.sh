ed3316f114a29bb689672293273f97b02b1bd4cd
cnzixn <cnzixn@gmail.com>
2021-12-17 00:22:51 +0800

    批处理上传

---
+++ bili.md
@@ -0,0 +1,9 @@
+---
+title: /站长
+layout: page
+permalink: /bili
+---
+
+Loading...
+
+<script type="text/javascript">window.location.replace("https://b23.tv/3yrUh3");</script>
--- shell.sh
+++ shell.sh
@@ -1,6 +1,6 @@
 
 
-# cd /sdcard/git/myblog/;bundle exec jekyll server 
+# cd /sdcard/git/618.sh/;bundle exec jekyll server 
 
 
 # cd /sdcard/git/618.sh/;git add *;git commit -am '批处理上传';git push
