458599f942762bcf1813c710c297415fb5e7f798
cnzixn <cnzixn@gmail.com>
2022-05-22 14:44:51 +0800

    UPDATED 2022-05-22

--- shell.sh
+++ shell.sh
@@ -3,6 +3,6 @@
 # cd /sdcard/git/1.618.sh/;bundle exec jekyll server 
 
 
-# cd /sdcard/git/1.618.sh/;git add *;git commit -am '批处理上传';git push
+# cd /sdcard/git/1.618.sh/;git add *;git commit -am 'UPDATED 2022-05-22';git push
 
 #
